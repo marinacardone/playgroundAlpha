c3cf83f29e392d58d85a93dfccd41fff94119e0d
Marina <marinacardone@gmail.com>
2013-09-29 14:39:37 -0300

    Desktop app test
    Creating an app to manage a beauty and pilates center to run locally on
    a desktop pc. Planning to use a js MV* (angular or backbone)

---
+++ calendario/_js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+	/* UX testing purpose - no logic applied yet */
+	$('#mainMenu a').on('click',function(){
+		var dataDisplay = $(this).attr('data-menuOpt');
+		
+		$('#mainMenu li').removeClass('active');
+		$(this).parent().addClass('active');
+
+		$('.page').addClass('hide');
+
+		switch(dataDisplay){
+			case 'calendarPilates':
+				$('#calendarApp').removeClass('hide');
+			break;
+			case 'calendarKinesio':
+				$('#calendarApp').removeClass('hide');
+			break;
+			case 'peopleList':
+				$('#personsList').removeClass('hide');
+			break;
+			case 'configuration':
+				$('#config').removeClass('hide');
+			break;
+			default:
+				$('#404').removeClass('hide');
+		}
+	});
+
+	$('.personModule a').on('click',function(){
+		$('.page').addClass('hide');
+		$('#person').removeClass('hide');
+	})
+
+	$('.personOptions a').on('click',function(){
+		var dataDisplay = $(this).attr('data-personAction');
+
+		switch(dataDisplay){
+			case 'view':
+				$('.content .page').addClass('hide');
+				$('#person').removeClass('hide');
+			break;
+			case 'edit':
+				$('.content .page').addClass('hide');
+				$('#person').removeClass('hide');
+			break;
+			case 'archive':
+				$(this).parents('tr').toggleClass('inactive');
+			break;
+			case 'remove':
+				$(this).parents('tr').addClass('hide');
+			break;
+			default:
+				$('#404').removeClass('hide');
+		}
+	})
+	
+	$('#home a').on('click',function(){
+		$('.page').addClass('hide');
+		$('#calendarApp').removeClass('hide');
+	});
+
+	$('.brand a').on('click',function(){
+		$('#mainMenu li').removeClass('active');
+		$('.page').addClass('hide');
+		$('#home').removeClass('hide');
+	});
+
+
+	$('.personModule :checkbox').on('change',function(){
+		$(this).parent().toggleClass('present');
+	})
+});
